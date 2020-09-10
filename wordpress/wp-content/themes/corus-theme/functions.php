571acf9edadd8b0ad8443df5417f9109b74cda0b
HerbertL <herbert.a.lemus@gmail.com>
2020-09-09 23:11:38 -0400

    added commments

--- wordpress/wp-content/themes/corus-theme/functions.php
+++ wordpress/wp-content/themes/corus-theme/functions.php
@@ -9,18 +9,17 @@ if ( ! defined( '_C_VERSION' ) ) {
  * Enqueue scripts and styles.
  */
 function corus_scripts() {
+    // enqueue slick styles
     wp_enqueue_style( 'slick-style', get_template_directory_uri() . '/components/slick-slider/slick.css');
     wp_enqueue_style( 'slick-theme-style', get_template_directory_uri() . '/components/slick-slider/slick-theme.css');
-    
-    // echo '<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>';
-    // echo '<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick-theme.css"/>';
 
-
+    // enqueue main style
     wp_enqueue_style( 'corus-style', get_stylesheet_uri(), array(), _C_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'corus_scripts', 10 );
 
 function body_js() {
+    // enqueue slick js and main js
 	wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/components/slick-slider/slick.js', array('jquery'), _C_VERSION, true );
     wp_enqueue_script( 'main-js', get_template_directory_uri() . '/components/js/main.js', array('jquery'), _C_VERSION, true );
 }
@@ -29,6 +28,7 @@ add_action('wp_footer', 'body_js', 10);
 // Our custom post type function
 function create_posttype() {
     
+    // create post type gallery
 	register_post_type( 'galleries',
 	    array(
             'labels' => array(
